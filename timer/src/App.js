02049c17c3f0f23bfc6a38777fed47d73c19d3e3
Luís Tchitue <ls04af@gmail.com>
2021-12-17 08:01:39 -0300

    creates the main component functions

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function SecondCounter(props) {
+
+}
+
+function SecondIncreaseButton() {
+
+}
+
+function SecondDecreaseButton() {
+
+}
+
+function MinuteCounter(props) {
+
+}
+
+function MinuteIncreaseButton() {
+
+}
+
+function MinuteDecreaseButton() {
+
+}
+
+function HourCounter() {
+
+}
+
+function HourIncreaseButton() {
+
+}
+
+function HourDecreaseButton() {
+
+}
+
+class App {
+  
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
