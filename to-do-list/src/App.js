ec2b4d9e68ce0b4b3f36ea40d5c0fece828b3323
Luís Tchitue <ls04af@gmail.com>
2021-12-20 14:05:01 -0300

    the to-do list app greets with a cute message

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Greeting (props) {
+  return <h1>
+    {props.value}
+  </h1>
+}
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      greet : 'hi again! Welcome back'
+    }
+  }
+
+  renderGreeting() {
+    return <Greeting value={this.state.greet}></Greeting>
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.renderGreeting()}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
