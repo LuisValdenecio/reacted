7bea512dbd64e0e022b010fc30a0ddf682e08f69
Luís Tchitue <ls04af@gmail.com>
2021-12-25 06:52:02 -0300

    creates and export data from a js file and shows it n the App class component

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import React, {Component} from 'react';
 import './App.css';
+import mockData from './mockData.js'
 
 class App extends Component {
 
+
   constructor(props) {
     super(props);
+    this.api = JSON.parse(mockData());
+
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Calendar App</h1>
+        <h1>{this.api.data}</h1>
       </div>
     );
 
